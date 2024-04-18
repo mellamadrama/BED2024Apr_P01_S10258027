938010d4afb7aef081eb2754d84a385f2a0ae612
mellamadrama <s10258027@connect.np.edu.sg>
2024-04-18 23:56:21 +0800

    prac2 changes

--- practical2.js
+++ practical2.js
@@ -1,2 +1,27 @@
 const express = require('express');
+const app = express();
+const port = 3000
 
+//app.METHOD(PATH, HANDLER);
+
+app.listen(3000, (req, res) => {
+    console.log('Express app listening on port ${3000}')
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.post('/', (req, res) => {
+    res.send('Got a POST request')
+});
+
+app.put('/', (req, res) => {
+    res.send('Got a PUT request at /')
+});
+
+app.delete('/', (req, res) => {
+    res.send('Got a DELETE request at /')
+});
+
+app.use(express.static('public'));
